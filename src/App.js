a912b04746fada4abfbca80e6d7f06713f500400
codeBindHex <xhunter19192020@gmail.com>
2022-09-11 15:14:48 +0200

    DispatchAuth

--- src/App.js
+++ src/App.js
@@ -1,25 +1,50 @@
 import "./App.css";
+import { useState } from "react";
 import { useSelector, useDispatch } from "react-redux";
 
-import { increase, decrease } from './store/counterSlice'
+import { increase, decrease } from "./store/counterSlice";
+import { logIn, logOut } from "./store/authSlice";
 
 function App() {
-  const globalState = useSelector((state) =>state)
-   const dispatch = useDispatch();
+  const globalState = useSelector((state) => state);
+  const dispatch = useDispatch();
+
+ 
+
+  const clickHandler = () => {
+ 
+    console.log(globalState.auth.isLoggedIn)
+    if (globalState.auth.isLoggedIn == true) {
+      dispatch(logOut())
+    }
+
+   
+    
+    if (globalState.auth.isLoggedIn == false) {
+      dispatch(logIn())
+    }
+  };
 
   return (
     <div className="App">
       <h1>Hello Redux Basic</h1>
+      {globalState.auth.isLoggedIn && (
+        <>
+          <div className="counter">Counter: {globalState.counter.value}</div>
+          <div>
+            <button className="btn" onClick={() => dispatch(increase(5))}>
+              increase +
+            </button>
+            <button className="btn" onClick={() => dispatch(decrease(2))}>
+              decrease -
+            </button>
+          </div>
+        </>
+      )}
 
-      <div className="counter">Counter: {globalState.counter.value}</div>
       <div>
-        <button className="btn" onClick={() => dispatch(increase(5))}>increase +</button>
-        <button className="btn" onClick={() => dispatch(decrease(2))}>decrease -</button>
-      </div>
-
-      <div>
-        <button className="btn">
-          Hide/Show Counter Number
+        <button className="btn" onClick={clickHandler}>
+          {globalState.auth.isLoggedIn ? "logout" : "login"}
         </button>
       </div>
     </div>
--- src/store/index.js
+++ src/store/index.js
@@ -1,8 +1,9 @@
 //**************************Redux ToolKit****************************
 import { configureStore } from "@reduxjs/toolkit";
 // configureStore => in order to combine multiple reducers
-import  counterSlice  from "./counterSlice";
+import counterSlice from "./counterSlice";
+import authSlice from "./authSlice";
 
-const store = configureStore({ reducer: { counter: counterSlice } });
+const store = configureStore({ reducer: { counter: counterSlice  , auth:authSlice} });
 
 export default store;
