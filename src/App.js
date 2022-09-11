82fcd8b533621c1e6eee0bf7f3e2fe83da9ebc3a
codeBindHex <xhunter19192020@gmail.com>
2022-09-11 11:24:28 +0200

    UpdateCounter

--- src/App.js
+++ src/App.js
@@ -1,85 +1,26 @@
 import "./App.css";
 import { useSelector, useDispatch } from "react-redux";
-import { useEffect, useCallback } from 'react';
+import { useEffect, useCallback } from "react";
+import { increase, decrease } from '../store/index'
+
 function App() {
-
-
-
+  const globalState = useSelector((state) => state);
   const dispatch = useDispatch();
 
-  const globalState = useSelector((state) => state);
-
-  const handlerCounterValue = (value) => {
-    if (value < 0) {
-       return  'no number'
-    }
-    
-    return value
-  }
-
-
-    // const counterState = useSelector((state) => {
-  //   return state.value;
-  // });
-
-  // const toggleState = useSelector((state) => {
-  //   return state.showCounter;
-  // });
-  const counterOperation = useCallback((type, payload) => {
-      dispatch({type , payload})
-    }
-    , [dispatch])
-  //useCallback كان 
-  //هي الل بتعقل يوز ايفكت تقولها خالي بالك هي نفس الفانكشن
-  //مترنيش تاني
-
-  // const counterOperation = (type, payload) => {
-  //   dispatch({type , payload})
-  // }
-
-  useEffect(() => {
-    counterOperation('increase' , 10)
-  }, [counterOperation])
-  
-  // in case i use useEffect only without usecallback the counter 
-  //counterOperationيوز ايفكت هتبقي غبيه وكل شويه هتروح تنفذ الفانكشن بتاعه
-//وبالتالي هتعمل re evaluate
-  //الحل اني استخدم يوزكولباك عشان اقوله خلي بالك هي نفس الفانكشن ليه بتتشغلي عمال علي بطال
-
-  // const increase = () => {
-  //   const action = { type: "increase", payload: 5 };
-  //   dispatch(action);
-  // };
-
-  // const decrease = () => {
-  //   const action = { type: "decrease", payload: 2 };
-  //   dispatch(action);
-  // };
-
-
-  const toggleCounter = () => {
-    const action = { type: 'toggleCounter' }
-    dispatch(action)
-  }
   return (
     <div className="App">
       <h1>Hello Redux Basic</h1>
-      {globalState.showCounter && (
-        <>
-          <div className="counter">Counter: {handlerCounterValue(globalState.value)}</div>
-          <div>
-            <button className="btn" onClick={() => counterOperation('increase' , 6)}>
-              increase +
-            </button>
-            <button className="btn" onClick={() =>  counterOperation('decrease' , 3)}>
-              decrease -
-            </button>
-          </div>
-        </>
-      )}
+
+      <div className="counter">Counter: {globalState.value}</div>
+      <div>
+        <button className="btn" onClick={() => dispatch(increase(5))}>increase +</button>
+        <button className="btn" onClick={() => dispatch(decrease(2))}>decrease -</button>
+      </div>
 
       <div>
-        <button className="btn" onClick={toggleCounter}>Hide/Show Counter Number</button>
+        <button className="btn">
+          Hide/Show Counter Number
+        </button>
       </div>
     </div>
   );
--- src/store/index.js
+++ src/store/index.js
@@ -1,52 +1,25 @@
-
-//**************************old Redux****************************
-// import { createStore } from 'redux';
-
-
-// const initState = { value: 0 , showCounter : true };
-
-// const counterReducer = (state = initState, action) => {
-//     if (action.type === "increase") {
-//         return {...state ,value :  state.value + action.payload}
-//     }
-    
-//     if (action.type === "decrease") {
-//         return {...state ,value : state.value  - action.payload }
-//     }
-
-//     if (action.type === 'toggleCounter') {
-//         return {...state ,  showCounter : !state.showCounter }
-//     }
-//     return state;
-// }
-
-// const store = createStore(counterReducer);
-
-//**************************old Redux****************************
-
 //**************************Redux ToolKit****************************
-import {createSlice , createStore } from '@reduxjs/toolkit';
+import { createSlice, createStore } from "@reduxjs/toolkit";
+import { useSelector, useDispatch } from 'react-redux';
+
 
 const initState = { value: 0 };
 
 const counterSlice = createSlice({
-    name: 'counter',
-    initialState: initState,
-    reducers: {
-        increase: (state, action) => {
-            state.value += 1;
-        },
-        decrease: (state, action) => {
-            state.value -= 1;
-        },
-    }
-   
+  name: "counter",
+  initialState: initState,
+  reducers: {
+    increase: (state, action) => {
+      state.value += action.payload;
+    },
+    decrease: (state, action) => {
+      state.value -= action.payload;
+    },
+  },
+});
 
-})
+const store = createStore(counterSlice.reducer);
 
-const store = createStore(counterSlice.reducer)
+export const { increase, decrease } = counterSlice.actions;
 
-
-
-
-export default store; 
+export default store;
