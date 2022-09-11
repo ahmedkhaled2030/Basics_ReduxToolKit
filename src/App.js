b33e7b420d511c3f3eeb937223f1d91df1d9a5e5
codeBindHex <xhunter19192020@gmail.com>
2022-09-11 15:48:18 +0200

    useCallback(dispatch)

--- src/App.js
+++ src/App.js
@@ -1,27 +1,50 @@
 import "./App.css";
-import { useState } from "react";
+import {  useEffect, useCallback } from "react";
 import { useSelector, useDispatch } from "react-redux";
 
 import { increase, decrease } from "./store/counterSlice";
 import { logIn, logOut } from "./store/authSlice";
 
+//1-useEffect
+//2-update State
+//3-re render
+//4-useEffect
+//عشان كدا لما بتسخدم يوزايفكت الكونتر بيعد كتير بالرغم ان الفانكشن في الاعتماد محصلهاش حاجه طيب اي السر
+// ************ re evaluate *********** السر
+// counterHandler ليها ref ثابت
+//لكن بيحصل ان كل مره بيرندر انه بيدي ريف جديد للفانكشن فبتتعرف تاني من الاخر الفانكشن بتتحور
+//فكدا دا يعتبر تغيير فيحصل dipatch
+//useCallback هنا بتقول لو سمحت يا رياكت محدش يعمل رن للفانكشن دي الا لما في حاجات جواها تتغير
+//عكس ذلك لا
+//التحديث الجديد في ريدكس حتي لو معملتش نفس الكلام هو بيهندله ممكن اعمل ديسباتش عالطول من يوزايفكت مش هيحصل مشكله ريافليوت
+
 function App() {
   const globalState = useSelector((state) => state);
   const dispatch = useDispatch();
 
- 
+  const counterHandler = useCallback(
+    (type, value) => {
+      if (type === "increase") {
+        dispatch(increase(value));
+      } else {
+        dispatch(decrease(value));
+      }
+    },
+    [dispatch]
+  );
+
+  useEffect(() => {
+    counterHandler("increase", 25);
+  }, [counterHandler]);
 
   const clickHandler = () => {
- 
-    console.log(globalState.auth.isLoggedIn)
+    console.log(globalState.auth.isLoggedIn);
     if (globalState.auth.isLoggedIn == true) {
-      dispatch(logOut())
+      dispatch(logOut());
     }
 
-   
-    
     if (globalState.auth.isLoggedIn == false) {
-      dispatch(logIn())
+      dispatch(logIn());
     }
   };
 
@@ -32,10 +55,16 @@ function App() {
         <>
           <div className="counter">Counter: {globalState.counter.value}</div>
           <div>
-            <button className="btn" onClick={() => dispatch(increase(5))}>
+            <button
+              className="btn"
+              onClick={() => counterHandler("increase", 20)}
+            >
               increase +
             </button>
-            <button className="btn" onClick={() => dispatch(decrease(2))}>
+            <button
+              className="btn"
+              onClick={() => counterHandler("decrease", 2)}
+            >
               decrease -
             </button>
           </div>
